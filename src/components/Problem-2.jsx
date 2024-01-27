5f8c7befa7dec713570b9dc36023e63c4258d5ab
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2024-01-27 12:50:43 +0600

    modal added

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,69 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 const Problem2 = () => {
+  const [showModalA, setShowModalA] = useState(false);
 
-    return (
+  const handleCloseModals = () => {
+    setShowModalA(false);
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg  btn-primary btn-outline-primary"
+            type="button"
+            onClick={() => setShowModalA(true)}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-warning btn-outline-warning"
+            type="button"
+            onClick={() => setShowModalA(true)}
+          >
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+
+      {/* Modal */}
+      <Modal show={showModalA} onHide={handleCloseModals}>
+        <Modal.Header closeButton>
+          <div className="d-flex justify-content-center gap-3">
+            <Button
+              style={{ backgroundColor: "#46139f", color:"#fff", borderColor: "#46139f" }}
+              className="btn btn-lg btn-outline-primary"
+              type="button"
+              onClick={() => setShowModalA(true)}
+            >
+              All Contacts
+            </Button>
+            <Button
+              style={{ backgroundColor: "#ff7f50", color:"#fff", borderColor: "#ff7f50" }}
+              className="btn btn-md btn-outline-warning"
+              type="button"
+              onClick={() => setShowModalA(true)}
+            >
+              US Contacts
+            </Button>
+            <Button
+              style={{ backgroundColor: "#46139f", color:"#fff", borderColor: "#46139f" }}
+              className="btn btn-md"
+              onClick={handleCloseModals}
+            >
+              Close
+            </Button>
+          </div>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+      </Modal>
+    </div>
+  );
 };
 
 export default Problem2;
